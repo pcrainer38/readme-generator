c250557c339ea5fe6cdb6e33a20f58d6ea393785
pcrainer38 <pammie_78704@yahoo.com>
2023-10-18 17:38:16 -0500

    updated link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,7 @@ function renderLicenseBadge(license) {
   if(!license) {
     return '';
   } else {
-    return `![badge](https://img.shields.io/badge/license-${license}-blue)
+    return `![badge](https://img.shields.io/badge/license-${license})
     `;
   }
 }
